7c7036edcf67203082db00a3d10a8ef88baf384e
Merit Eller <merit@snowhound.eu>
2018-10-01 18:57:05 +0300

    added catch for axios

--- src/api/QuestionsService.js
+++ src/api/QuestionsService.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 export const getQuestions = () => {
   const URL = 'https://blog.kmu.ee/wp-json/wp/v2/questions';
   return axios.get(URL)
-    .then((response) => response.data);
+    .then((response) => response.data)
+    .catch(err => console.log(err));
 };
