3e60e401d1b3667207fdef8994c37e673375c638
Merit Eller <merit@snowhound.eu>
2018-10-01 18:19:42 +0300

    removed old code from App

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import QuestionFormContainer from '../containers/QuestionFormContainer';
-import { getQuestions } from '../api/QuestionsService';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <QuestionFormContainer />
-        {getQuestions()}
-      </div>
+      <QuestionFormContainer />
     );
   }
 }
