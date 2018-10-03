b3880eaf649216e952a7a584f56e08ebb48bd9d5
Kadi Kukk <kadikukk10@gmail.com>
2018-10-03 17:00:15 +0300

    Update material-ui to version 3.1.2

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider } from 'material-ui/styles';
+
 import QuestionFormContainer from '../containers/QuestionFormContainer';
 
 class App extends Component {
-	render() {
-		return (
-			<QuestionFormContainer />
-		);
-	}
+  render() {
+    return (
+      <MuiThemeProvider>
+        <QuestionFormContainer />
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
