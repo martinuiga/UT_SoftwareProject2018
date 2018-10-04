7e20eeb0bd3d96f0f92a8b4d686b8d5e307b87c8
Merit Eller <merit@snowhound.eu>
2018-10-04 10:13:47 +0300

    WIP: questions data structurized

--- src/api/QuestionsService.js
+++ src/api/QuestionsService.js
@@ -1,12 +1,37 @@
 import axios from 'axios';
 
+const structurizeQuestions = (APIresponseData) => {
+  const questions = [];
+  APIresponseData.forEach(question => {
+    const questionObject = {
+      question: question.title.rendered,
+      type: question.slug
+    };
+    let answerChoices;
+    switch (questionObject.type) {
+      case 'single-select-question':
+        answerChoices = question.acf.single_select_choices.split(';');
+        break;
+      case 'multiple-select-question':
+        answerChoices = question.acf.multiple_select_choices.split(';');
+        break;
+      default:
+        break;
+    }
+    questions.push(questionObject);
+  });
+  console.log('QUESTIONS STRUCTURIZED', questions);
+  return questions;
+};
+
 export const getQuestions = () => {
   const URL = 'https://blog.kmu.ee/wp-json/wp/v2/questions';
 
+
   return axios.get(URL)
     .then((response) => {
-      // console.log('response.data', response.data);
-      return response.data;
+      console.log('response.data', response.data);
+      return structurizeQuestions(response.data);
     })
     .catch(err => console.log(err));
 };
--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import QuestionFormContainer from '../containers/QuestionFormContainer';
+import { getQuestions } from '../api/QuestionsService';
 
 class App extends Component {
   render() {
+    console.log('getQuestions', getQuestions());
     return (
       <QuestionFormContainer />
     );
