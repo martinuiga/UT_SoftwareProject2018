735b32f117541e83d4575f240ad53a6f232e7c48
Merit Eller <merit@snowhound.eu>
2018-10-04 11:39:30 +0300

    initial sturcture for rendering correct answer component

--- src/questions/QuestionsForm.js
+++ src/questions/QuestionsForm.js
@@ -5,10 +5,36 @@ import AnswerOptions from '../answers/AnswerOptions';
 class QuestionsForm extends React.Component {
   // Siia tuleb loogika, mis võtab ühe välja kuvatava küsimuse
   // ja vastavalt tüübile renderdatakse õiged komponendid
+  renderCorrectAnswerOptions = (question) => {
+    switch (question.type) {
+      case 'single-select-question':
+        return (
+          <SingleSelect
+            question={question}
+          />
+        );
+      case 'multiple-select-question':
+        return (
+          <MultipleSelect
+            question={question}
+          />
+        );
+      case 'short-answer-question':
+        return (
+          <ShortAnswer
+            question={question}
+          />
+        );
+      default:
+        return '';
+    }
+  }
+
   render() {
     return (
       <div style={{ textAlign: 'center' }}>
         <Question />
+        {this.renderCorrectAnswerOptions()}
         {/* Siin tuleb välja vaadata, millise küsimusega on tegemist
         ja AnswersOptions asemele teha mingi case'ide lugu, mis renderdab õige komponendi */}
         <AnswerOptions />
