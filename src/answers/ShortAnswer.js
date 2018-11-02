dc903cdb94714c9ecedb3d02024039d17ba88e3e
Merit Eller <merit@snowhound.eu>
2018-11-02 17:26:52 +0200

    textField value gets saved into state

--- src/answers/ShortAnswer.js
+++ src/answers/ShortAnswer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { merge } from 'ramda';
 import TextField from '@material-ui/core/TextField';
 
 const MAXIMUM_CHARACTER_COUNT = 150;
@@ -6,10 +7,24 @@ const helperText = 'Vastus võib olla maksimaalselt ' +
   MAXIMUM_CHARACTER_COUNT +
   ' tähemärgi pikkune';
 class ShortAnswer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answerText: ''
+    };
+  }
+
+  handleAnswerTextChange = (event) => {
+    event.persist();
+    this.setState((prevState) => merge(prevState, { answerText: event.target.value }));
+  }
+
   render() {
     return (
       <TextField
         id="shortTextAnswer"
+        value={this.state.answerText}
+        onChange={this.handleAnswerTextChange}
         label="Vastus"
         multiline
         rows="4"
