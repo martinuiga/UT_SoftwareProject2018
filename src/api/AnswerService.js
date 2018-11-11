1ce16d390692d881dd285cbf69e0c62e41e9ac94
Kadi Kukk <kadikukk10@gmail.com>
2018-11-11 12:34:38 +0200

    Load answers to questions to state

--- src/api/AnswerService.js
+++ src/api/AnswerService.js
@@ -1,8 +1,31 @@
-import Axios from 'axios';
+import axios from 'axios';
+
+
+export const structurizeAnswers = (APIresponseData) => {
+  const answers = [];
+  APIresponseData.forEach(question => {
+    const answerObject = {
+      question: question.term_question,
+      answer: question.term_answer
+    };
+    answers.push(answerObject);
+  });
+  return answers;
+};
+
+export const getAnswers = () => {
+  const URL = 'https://blog.kmu.ee/wp-json/db-json/questions';
+
+  return axios.get(URL)
+    .then((response) => {
+      return structurizeAnswers(response.data);
+    })
+    .catch(err => console.log(err));
+};
 
 export const sendAnswer = (answer) => {
   const URL = 'https://blog.kmu.ee/wp-json/db/addquestion';
 
-  return Axios.post(URL, answer)
+  return axios.post(URL, answer)
     .catch(err => console.log(err));
 };
